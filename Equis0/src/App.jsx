76cc28c04bf7ca943c2ff25d7394fbe616a63067
abraham55github <155010549+abraham55github@users.noreply.github.com>
2024-10-22 22:21:38 -0500

    Agregando tablero de equis0

--- Equis0/src/App.jsx
+++ Equis0/src/App.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import { useState } from "react";
+
+
+const Turnos = {
+  X : 'x', 
+  O : 'o'
+}
+
+
+
+const Square = ({ children, updateBoard, index }) => {
+  return (
+    <div className="square">
+      {children}
+    </div>
+  )
+}
 
 export default function App() {
+  const board = Array(9).fill(null);
+  const [boardx, setBoardx] = useState()
+
+
   return (
-    <div>
-      
-    </div>
+    <main className="board">
+      <h1> Juego X y 0</h1>
+      <section className="game">
+        {
+        board.map((cell, index) => {
+          return (
+            <Square 
+              key={index} 
+              index={index}
+              
+            >
+
+            </Square>
+          )
+        })
+        }
+      </section>
+    </main>
   )
 }

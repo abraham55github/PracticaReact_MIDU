4af3326c50acd0f4fb33ca690e6ac2dd8c17568b
abraham55github <155010549+abraham55github@users.noreply.github.com>
2024-10-23 23:39:28 -0500

    a;adiendo updateboard

--- Equis0/src/App.jsx
+++ Equis0/src/App.jsx
@@ -35,6 +35,7 @@ export default function App() {
             <Square 
               key={index} 
               index={index}
+              updateBoard={}
               
             >
               {board[index]}

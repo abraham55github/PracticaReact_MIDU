2c3c0f06e1d48b3816e9a1076cb69e752f1e0c56
abraham55github <155010549+abraham55github@users.noreply.github.com>
2024-10-24 22:27:36 -0500

    Ultimas partes del juego, creamos la logica para ganador y eleccion de ganador

--- Equis0/src/App.jsx
+++ Equis0/src/App.jsx
@@ -2,8 +2,8 @@ import { useState } from "react";
 
 
 const Turnos = {
-  X : 'x', 
-  O : 'o'
+  X: 'x',
+  O: 'o'
 }
 
 
@@ -23,17 +23,59 @@ const Square = ({ children, isSelected, updateBoard, index }) => {
   )
 }
 
+const WINER_COMBOS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+];
+
 export default function App() {
 
   const [board, setBoard] = useState(Array(9).fill(null))
   const [turno, setTurno] = useState(Turnos.X)
+  const [winner, setWinner] = useState(null)
+
+  const checkWinner = (boardToCheck) => {
+    for (const combo of WINER_COMBOS) {
+      const [a, b, c] = combo;
+      if (
+        boardToCheck[a] &&
+        boardToCheck[a] === boardToCheck[b] &&
+        boardToCheck[b] === boardToCheck[c]) {
+        return boardToCheck[a];
+      }
+    }
+  }
+
+  const resetGames = () => {
+    setBoard(Array(9).fill(null))
+    setTurno(Turnos.X)
+    setWinner(null)
+  }
 
   const updateBoard = (index) => {
-    const newBoard = [... board]
+    // no actualizamos esta posicion
+    // si ya tiene algo
+    if (board[index] || winner) return
+    const newBoard = [...board]
+    // actualizar el tablero
     newBoard[index] = turno
     setBoard(newBoard)
+    //cambiar el turno
     const nuevoTurno = turno === Turnos.X ? Turnos.O : Turnos.X;
     setTurno(nuevoTurno)
+    //revisar si hay ganador
+    const newWinner = checkWinner(newBoard);
+    if (newWinner) {
+      setWinner(newWinner)
+    }else if (setBoard(newBoard)) {
+      setWinner(false)
+    }
   }
 
   return (
@@ -41,25 +83,52 @@ export default function App() {
       <h1>Juego</h1>
       <section className="game">
         {
-        board.map((cell, index) => {
-          return (
-            <Square 
-              key={index} 
-              index={index}
-              updateBoard={updateBoard}
-              
-            >
-              {board[index]}
-            </Square>
-          )
-        })
+          board.map((cell, index) => {
+            return (
+              <Square
+                key={index}
+                index={index}
+                updateBoard={updateBoard}
+
+              >
+                {board[index]}
+              </Square>
+            )
+          })
         }
       </section>
+      <footer>
+        <button onClick={resetGames}>Reiniciar juego</button>
+      </footer>
 
       <section className="turn">
         <Square isSelected={turno === Turnos.X}>{Turnos.X}</Square>
         <Square isSelected={turno === Turnos.O}>{Turnos.O}</Square>
       </section>
+
+
+      {
+        winner !== null && (
+          <section className="winner">
+            <div className="text">
+              <h2>
+                {
+                  winner === false ? 'Empate' : 'Gano: '
+                }
+              </h2>
+              <header className="win">
+                {winner && <Square>{winner}</Square>}
+
+              </header>
+
+
+              <footer>
+                <button onClick={resetGames}>Empezar de nuevo</button>
+              </footer>
+            </div>
+          </section>
+        )
+      }
     </main>
   )
 }

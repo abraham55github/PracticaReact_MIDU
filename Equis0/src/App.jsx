2f85ca1eeba122757bfd212ec4ea70fabc5e925b
abraham55github <155010549+abraham55github@users.noreply.github.com>
2024-10-28 17:54:14 -0500

    componetizando el proyecto

--- Equis0/src/App.jsx
+++ Equis0/src/App.jsx
@@ -1,38 +1,7 @@
 import { useState } from "react";
+import { Square } from "./components/Square";
+import { WINER_COMBOS, Turnos } from "./constants";
 
-
-const Turnos = {
-  X: 'x',
-  O: 'o'
-}
-
-
-
-const Square = ({ children, isSelected, updateBoard, index }) => {
-
-  const className = `square ${isSelected ? 'is-selected' : ''}`
-
-  const handleClick = () => {
-    updateBoard(index)
-  }
-
-  return (
-    <div onClick={handleClick} className={className}>
-      {children}
-    </div>
-  )
-}
-
-const WINER_COMBOS = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6]
-];
 
 export default function App() {
 

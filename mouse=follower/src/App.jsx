a6ff393d104284766898641ed761e02d171e89f2
abraham55github <155010549+abraham55github@users.noreply.github.com>
2024-11-06 22:50:30 -0500

    creando circulo dentro del div para perseguir el raton con useeffect

--- mouse=follower/src/App.jsx
+++ mouse=follower/src/App.jsx
@@ -1,13 +1,39 @@
-
-import './App.css'
+import { useEffect, useState } from "react"
 
 function App() {
 
+  const [enable, setEnable] = useState(false);
+
+  useEffect(() => {
+    const handlemoved = (e) -> {
+      const {clientX, clientY} = e
+    }
+  }, [])
+
+  const handleClick = () => {
+    setEnable(!enable)
+  }
 
   return (
-    <h1>
-      Proyecto 3
-    </h1>
+
+    <main>
+      <div style={{
+        position: 'absolute',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        border: '1px solid #fff',
+        borderRadius: '50%',
+        opacity: 0.8,
+        pointerEvents: 'none',
+        left: -25,
+        top: -25,
+        width: 50,
+        height: 50,
+        transform: `translate(${position.x}px, ${position.y}px)`
+      }}
+      />
+      <button onClick={handleClick}>{enable ? 'Desactivar' : 'Activar'} seguir puntero</button>
+
+    </main >
   )
 }
 

0604fd2e9ea818b9bcfa610948769c2a29888106
abraham55github <155010549+abraham55github@users.noreply.github.com>
2024-11-22 23:00:30 -0500

    creand a,biente para el proyecto

--- ajedres/src/components/Square.jsx
+++ ajedres/src/components/Square.jsx
@@ -1,19 +1,12 @@
-export const Square = ({ children, isSelected, updateBoard, index }) => {
 
-    const col = index % 8
-    const row = Math.floor(index / 8)
-    
-    const isWhite = (row % 2 === 0 && col % 2 === 0)  || (row % 2 !== 0 && col % 2 !== 0)
 
-    const className = `square ${isSelected ? 'is-selected' : ''} ${isWhite ? 'white' : 'black'}`
-   
-    const handleClick = () => {
-      updateBoard(index)
-    }
+export const Square = () => {
+
+
 
     return (
-      <div onClick={handleClick} className={className}>
-        {children}
-      </div>
+      <>
+
+      </>
     )
   }

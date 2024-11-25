0242823e29e7cb03fd0a650afd4c213dfca97679
abraham55github <155010549+abraham55github@users.noreply.github.com>
2024-11-24 22:54:57 -0500

    commit de square

--- ajedres/src/components/Square.jsx
+++ ajedres/src/components/Square.jsx
@@ -1,18 +1,18 @@
 import Pieces from "./Pieces"
 
-export const Square = ({square, color, IsSelected}) => {
+export const Square = ({ square, color, IsSelected }) => {
 
-    const squareColor = square.color || (square.index % 2 === 0 ? 'light' : 'dark')
+  const squareColor = square.color || (square.index % 2 === 0 ? 'light' : 'dark')
 
 
-    return (
-      <>
+  return (
+    <>
       <div
-      className={`square ${squareColor} ${IsSelected ? 'highlight' : ''}`}
+        className={`square ${squareColor} ${IsSelected ? 'highlight' : ''}`}
       >
         {square.piece && <Pieces type={square.piece.type} color={square.piece.color} />}
       </div>
 
-      </>
-    )
-  }
+    </>
+  )
+}

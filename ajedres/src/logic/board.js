e6e1f6533fc58468fde8881a1675dd102343564c
abraham55github <155010549+abraham55github@users.noreply.github.com>
2024-11-29 23:15:30 -0500

    componetizar piezas

--- ajedres/src/logic/board.js
+++ ajedres/src/logic/board.js
@@ -16,13 +16,45 @@ export const checkEndGame = (newBoard) => {
     return newBoard.every((Square) => Square !== null)
 } */
 
-export const setupInitialBoard = () => {
+/* export const setupInitialBoard = () => {
   const board = Array(8).fill(null).map(() => Array(8).fill(null))
   board[0] = ["♜", "♞", "♝", "♛", "♚", "♝", "♞", "♜"];
   board[1] = Array(8).fill("♟");
 
   board[6] = Array(8).fill("♙");
   board[7] = ["♖", "♘", "♗", "♕", "♔", "♗", "♘", "♖"];
+
+  return board;
+}; */
+
+export const setupInitialBoard = () => {
+  const board = Array(8).fill(null).map(() => Array(8).fill(null));
+
+  // Piezas negras
+  board[0] = [
+    { type: "♜", color: "black" },
+    { type: "♞", color: "black" },
+    { type: "♝", color: "black" },
+    { type: "♛", color: "black" },
+    { type: "♚", color: "black" },
+    { type: "♝", color: "black" },
+    { type: "♞", color: "black" },
+    { type: "♜", color: "black" },
+  ];
+  board[1] = Array(8).fill({ type: "♟", color: "black" });
+
+  // Piezas blancas
+  board[6] = Array(8).fill({ type: "♙", color: "white" });
+  board[7] = [
+    { type: "♖", color: "white" },
+    { type: "♘", color: "white" },
+    { type: "♗", color: "white" },
+    { type: "♕", color: "white" },
+    { type: "♔", color: "white" },
+    { type: "♗", color: "white" },
+    { type: "♘", color: "white" },
+    { type: "♖", color: "white" },
+  ];
 
   return board;
 };

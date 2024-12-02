d8e71afaffa20fb88b0cbfbfab6362892f5c1541
abraham55github <155010549+abraham55github@users.noreply.github.com>
2024-12-01 19:03:42 -0500

    mas arreglos a los estilos de index

--- ajedres/src/logic/board.js
+++ ajedres/src/logic/board.js
@@ -44,16 +44,16 @@ export const setupInitialBoard = () => {
   board[1] = Array(8).fill({ type: "♟", color: "black" });
 
   // Piezas blancas
-  board[6] = Array(8).fill({ type: "♙", color: "white" });
+  board[6] = Array(8).fill({ type: "♟", color: "white" });
   board[7] = [
-    { type: "♖", color: "white" },
-    { type: "♘", color: "white" },
-    { type: "♗", color: "white" },
-    { type: "♕", color: "white" },
-    { type: "♔", color: "white" },
-    { type: "♗", color: "white" },
-    { type: "♘", color: "white" },
-    { type: "♖", color: "white" },
+    { type: "♜", color: "white" },
+    { type: "♞", color: "white" },
+    { type: "♝", color: "white" },
+    { type: "♛", color: "white" },
+    { type: "♚", color: "white" },
+    { type: "♝", color: "white" },
+    { type: "♞", color: "white" },
+    { type: "♜", color: "white" },
   ];
 
   return board;

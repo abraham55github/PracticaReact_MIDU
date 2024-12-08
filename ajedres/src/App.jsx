bb06f90928b245cbd86931db62d9b22b56fd0d19
abraham55github <155010549+abraham55github@users.noreply.github.com>
2024-12-08 00:06:22 -0500

    empezando con nuecvas funciones

--- ajedres/src/App.jsx
+++ ajedres/src/App.jsx
@@ -14,40 +14,6 @@ export default function App() {
     setTurn((prevTurn)=> (prevTurn === "white" ? "black" : "white"));
   }
 
-  const handleSquareClick = (rowIndex, colIndex) => {
-    const piece = board[rowIndex][colIndex];
-
-  };
-
-  // Función para calcular los movimientos válidos de una pieza
-  const getValidMoves = (piece, rowIndex, colIndex, board) => {
-    // Lógica para los movimientos válidos según el tipo de pieza
-    let moves = [];
-    switch (piece.type) {
-      case "♟": // Peón
-        moves = getPawnMoves(piece, rowIndex, colIndex, board);
-        break;
-      case "♞": // Caballo
-        moves = getKnightMoves(rowIndex, colIndex, board);
-        break;
-      case "♝": // Alfil
-      
-        break;
-      case "♜": // Torre
-      
-        break;
-      case "♛": // Reina
- 
-        break;
-      case "♚": // Rey
- 
-        break;
-      default:
-        break;
-    }
-    return moves;
-  };
-
 
   return (
 <main className="board">
@@ -61,7 +27,6 @@ export default function App() {
               <Square
                 key={`${rowIndex}-${colIndex}`}
                 color={isBlack ? "black" : "white"}
-                onClick={() => handleSquareClick(rowIndex, colIndex)}
               >
                 {cell && <Pieces type={cell.type} color={cell.color} />}
               </Square>

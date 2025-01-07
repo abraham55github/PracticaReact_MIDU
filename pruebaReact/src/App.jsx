cc81b83b9ed148eaa0fade0678fcadfd1dd5ec84
abraham55github <155010549+abraham55github@users.noreply.github.com>
2025-01-06 22:44:27 -0500

    añadiendo endpoint para el fetch tengo que buscar el api de image_url

--- pruebaReact/src/App.jsx
+++ pruebaReact/src/App.jsx
@@ -1,7 +1,24 @@
+import { useEffect, useState } from "react"
+
+const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
+const CAT_ENDPOINT_IMAGE_URL = 'https://catfact.ninja/fact'
+
 export function App() {
+    const [fact, setFact] = useState('lorem ipsum dolor sit amet, consectetur adipiscing');
+
+    useEffect(() => {
+        fetch(CAT_ENDPOINT_RANDOM_FACT)
+        .then(res => res.json())
+        .then(data => setFact(data.fact))
+    })
+
+
     return (
-        <div>
-            App de gatitos
-        </div>
+        <main>
+            <h1>App de gatitos</h1>
+            <p>{fact}</p>
+
+        </main>
+
     )
 }

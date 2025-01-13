fe1c516b42bc8f1b9c0dd1fdf4f855b2895cbabb
abraham55github <155010549+abraham55github@users.noreply.github.com>
2025-01-12 21:58:47 -0500

    otro fetch para capturar nuevo endpoiunt

--- pruebaReact/src/App.jsx
+++ pruebaReact/src/App.jsx
@@ -5,18 +5,33 @@ const CAT_ENDPOINT_IMAGE_URL = 'https://catfact.ninja/fact'
 
 export function App() {
     const [fact, setFact] = useState('lorem ipsum dolor sit amet, consectetur adipiscing');
+    const [imageUrl, setImageUrl] = useState()
 
     useEffect(() => {
         fetch(CAT_ENDPOINT_RANDOM_FACT)
         .then(res => res.json())
-        .then(data => setFact(data.fact))
+        .then(data => {
+            const { fact } = data
+            setFact(fact) 
+            
+            const threeFirstWords = fact.split(' ', 3).join('')
+            console.log(threeFirstWords)
+
+            fetch(`hhtps://cataas.com/cat/says/${threeFirstWords}?size=50&color=red&json=true`)
+            .then(res => res.json())
+            .then(response => {
+                const { url } = response
+            })
+        })
     }, [])
 
 
     return (
         <main>
             <h1>App de gatitos</h1>
-            <p>{fact}</p>
+            { fact && <p>{fact}</p>}
+            {imageUrl && <img src={imageUrl} alt={`Image extracted using the first three word for ${fact}`}/>}
+            
 
         </main>
 
